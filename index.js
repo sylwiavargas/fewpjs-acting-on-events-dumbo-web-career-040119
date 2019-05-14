8cbf2a5779058e97566b27a73fab09c6b1896d73
Sylwia Vargas <sylwia.vargas@gmail.com>
2019-05-13 23:56:45 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Your code here
+const dodger = document.getElementById("dodger")
+
+dodger.style.backgroundColor = 'lime'
+
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace('px', '')
+    const left = parseInt(leftNumbers, 10)
+
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`
+    }
+  }
+
+  document.addEventListener('keydown', function(event) {
+    if (event.which === 39) {
+      moveDodgerRight()
+    }
+  })
+
+  function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace('px', '')
+    const left = parseInt(leftNumbers, 10)
+
+    if (left < 360) {
+      dodger.style.left = `${left + 1}px`
+    }
+  }
